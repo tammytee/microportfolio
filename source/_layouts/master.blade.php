9867d050748ef95515df981ebe36cdfcbf019451
Tammy Robinson <tammyrobinson@Tammys-Tgtn-MacBook-Pro.local>
2020-02-12 19:10:37 -0500

    add in title meta

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-    </head>
-    <body>
-        @yield('body')
-    </body>
+  <head>
+    <title>Tammy Robinson | Developer, Visionary, and Community Organizer based in Charlotte, NC</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+  </head>
+  <body>
+    @yield('body')
+  </body>
 </html>
